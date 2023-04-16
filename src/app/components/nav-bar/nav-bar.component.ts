1a56479819bb8d96fca0fee896bdfdd3cb745526
Quinn Johnson <quinnjohnson24@gmail.com>
2023-04-15 19:58:59 -0500

    added auth0 and env variables

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 @Component({
   selector: 'app-navbar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent {
+  constructor(public auth: AuthService) {}
+
 }
